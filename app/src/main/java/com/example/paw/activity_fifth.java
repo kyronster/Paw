fc21af0094f993804989627d56f6d1fd374c5feb
kyronster <mkrenido@tip.edu.ph>
2023-05-07 13:04:07 +0800

    Second Commit

---
+++ app/src/main/java/com/example/paw/activity_fifth.java
@@ -0,0 +1,14 @@
+package com.example.paw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_fifth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth);
+    }
+}
